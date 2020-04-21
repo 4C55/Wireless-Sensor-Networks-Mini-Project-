9e4508e56f83df1f675ef4151d6c3a39eb168d36
Laurynas <laurynas.ubys@gmail.com>
2020-04-21 02:23:49 +0200

    Finally got data upload/download working. Requires lower baud rate

--- Python/Message/Message.py
+++ Python/Message/Message.py
@@ -1,8 +1,7 @@
 import MessageType
 import MessageAddress
 
-SIZE_OF_MESSAGE_DATA = 32
-
+SIZE_OF_MESSAGE_DATA = 249
 
 class Message:
     def __init__(
@@ -10,12 +9,15 @@ class Message:
             message_type=MessageType.MESSAGE_TYPE_EMPTY,
             source=MessageAddress.MESSAGE_ADDRESS_PC,
             destination=MessageAddress.MESSAGE_ADDRESS_MOTE,
-            data=[]):
+            data=[],
+            attempts=3,
+            timeout=3):
         self.message_type = message_type
         self.source = source
         self.destination = destination
         self.data = data
-
+        self.attempts = attempts
+        self.timeout = timeout
 
 
 
--- Python/UploadImage.py
+++ Python/UploadImage.py
@@ -6,24 +6,36 @@ from Message import Message
 def main():
     handler = MessageHandler('COM8')
 
-    # message = Messages.TestRequest()
-    # reply = handler.send_and_wait(message, MessageType.MESSAGE_TYPE_TEST_RESPONSE)
-    # print(reply)
+    data = []
+    for i in range(240):
+        data.append(i)
 
-    # data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    # message = Messages.WriteRequest(
-    #     address=0,
-    #     write_data=data)
-    # reply = handler.send_and_wait(message, MessageType.MESSAGE_TYPE_WRITE_RESPONSE)
-    # print(reply)
+    # Format memory
+    message = Messages.FormatRequest()
+    reply = handler.send_and_wait(message, MessageType.MESSAGE_TYPE_FORMAT_RESPONSE)
+    if reply is None or not reply.success:
+        return
 
-    message = Messages.ReadRequest(
-        address=5,
-        length=5)
-    reply = handler.send_and_wait(message, MessageType.MESSAGE_TYPE_READ_RESPONSE)
-    print(reply)
-    print(reply.read_data)
+    # Upload
+    for i in range(271):
+        message = Messages.WriteRequest(
+            address=len(data) * i,
+            write_data=data)
+        reply = handler.send_and_wait(message, MessageType.MESSAGE_TYPE_WRITE_RESPONSE)
+        print(len(data) * i)
+        if reply is None or not reply.success:
+            return
 
+    # Read
+    for i in range(271):
+        message = Messages.ReadRequest(
+            address=len(data) * i,
+            length=len(data))
+        reply = handler.send_and_wait(message, MessageType.MESSAGE_TYPE_READ_RESPONSE)
+        if reply is None:
+            return
+        if reply is not None:
+            print(len(data) * i)
 
     handler.dispose()
 
